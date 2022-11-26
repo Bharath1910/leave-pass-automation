53d50dde001aeaa44b866833c7e252b5bd41b328
Bharath1910 <av.bharath@proton.me>
2022-11-26 23:42:38 +0530

    migrating to mongodb

--- server/main.py
+++ server/main.py
@@ -1,24 +1,14 @@
-import firebase_admin, datetime
-from firebase_admin import credentials
-from firebase_admin import firestore
+from dotenv import  load_dotenv, find_dotenv
+from pymongo import MongoClient
+import os, pprint
 
-# Use a service account.
-cred = credentials.Certificate('./firebase-adminsdk.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+load_dotenv(find_dotenv())
 
-docRef = db.collection(u'declined').document(u'22BEC7194')
-docRef.set({
-    u'regNo': u'22BEC7194',
-    u'fromDate': datetime.datetime(2020, 5, 1),
-    u'toDate': datetime.datetime(2022, 5, 1),
-    u'reason': u'Festival',
-    u'mentorRegNo': u'22ABC1234'
-})
+# Loading the environment variables
+dbUsr = os.environ.get("MONGODB_USR")
+dbPwd = os.environ.get("MONGODB_PWD")
 
-users_ref = db.collection(u'pending')
-docs = users_ref.stream()
-
-for doc in docs:
-    print(f'{doc.id} => {doc.to_dict()}')
+# Connecting to the database
+connectionString = f"mongodb+srv://{dbUsr}:{dbPwd}@cluster0.w25vf.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connectionString)
 
