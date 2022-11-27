921c529de44ec30d734cc547e999dd7599e5f2f5
Bharath1910 <av.bharath@proton.me>
2022-11-27 12:20:48 +0530

    completed the sphegetti code for the project

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 from dotenv import  load_dotenv, find_dotenv # To load environment variables
 from pymongo import MongoClient # MongoDB API
 import RPi.GPIO as gpio # To access GPIO pins on raspberry pi
-import os # To access environment variables
+import os, datetime 
 
 # Setting up GPIO in raspberry pi
 gpio.setmode(gpio.BOARD)
@@ -38,17 +38,44 @@ def rfidData(temp):
     else:
         return "19BCE1234"
 
+def daysLeft(dateStr):
+    curDate = datetime.datetime.now()
+    #dateStrObj = datetime.datetime.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
+    leftDays = dateStr - curDate
+
+    return leftDays
+
+def isLate(regNo, collection):
+    scn = collection.find_one({"regNo": regNo})
+    retrunDate = scn["toDate"]
+    curDate = datetime.datetime.now()
+
+    return retrunDate < curDate
+    if delDate.days >= 0:
+        return False
+
+    elif delDate.days < 0:
+        return True
+
+
+
 gpio.setup(8, gpio.IN)
 print("starting")
 while True:
     try:
         if gpio.input(8):
-            data = rfidData(1)
+            data = rfidData(2)
             dbData = isScanned(approvedCollection ,data)
             if dbData:
-                print(f"Welcome Home {data} \nyou left the campus on: {dbData['lastScanned']}")
+                print(f"Welcome Home {data} \nyou were on leave for: {daysLeft(dbData['lastScanned'])} days\nAre you late?: {isLate(data, approvedCollection)}")
             
             else:
+                curTime = datetime.datetime.now()
+                updateStr = {
+                    "$set": {"lastScanned": curTime}
+                }
+
+                approvedCollection.update_one({"regNo": data}, updateStr)
                 print("Happy Journey")
         
         else:
